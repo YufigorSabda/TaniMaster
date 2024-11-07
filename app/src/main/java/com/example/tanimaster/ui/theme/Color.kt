d3350d0aea71620df4713e13ed05b30e84c04a3b
BotSlayer21 <t21rizkianggara@gmail.com>
2024-11-07 11:33:31 +0700

    Modify folder structure

--- app/src/main/java/com/example/tanimaster/ui/theme/Color.kt
+++ app/src/main/java/com/example/tanimaster/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.tanimaster.ui.theme.theme
+package com.example.tanimaster.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/main/java/com/example/tanimaster/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tanimaster/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.tanimaster.ui.theme.theme
+package com.example.tanimaster.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
