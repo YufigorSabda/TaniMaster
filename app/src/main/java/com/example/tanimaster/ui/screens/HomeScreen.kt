6e4d0bf18ad867f7a5c5d71e7f90694673c0f2bd
BotSlayer21 <t21rizkianggara@gmail.com>
2024-11-07 22:54:41 +0700

    Add HomeScreen, TopAppBar, BottomAppBar

--- app/src/main/java/com/example/tanimaster/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/tanimaster/ui/screens/HomeScreen.kt
@@ -1,11 +1,10 @@
-package com.example.tanimaster.ui.theme.screens
+package com.example.tanimaster.ui.screens
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
